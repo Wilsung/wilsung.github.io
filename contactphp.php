fadbae3c710e1e22e208754e710d81c418cc8c6e
Wilson <wou123@yahoo.com>
2018-02-08 22:27:40 -0800

    finished first draft of the website - added, about section, contact section, and portfolio section.

---
+++ contactphp.php
@@ -0,0 +1,16 @@
+<?php
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$from = "From: ".$email;
+	$to = 'wou123@yahoo.com';
+	$subject = $_POST['subject'];
+
+	$body = "From: $first_name $last_name\n E-mail: $email\n Message:\n $message";
+
+	if (isset($_POST['submit'])){
+		mail($to, $subject, $body, $from);
+		header("Location: contact.html?mailsend");
+	}
+?>
